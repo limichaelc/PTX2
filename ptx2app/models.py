f4bf76af830b297920b13062e11ae854d139d6f3
Alan <ayzhou@princeton.edu>
2014-04-06 21:58:50 -0400

    fixed book bug 2

--- ptx2app/models.py
+++ ptx2app/models.py
@@ -15,7 +15,7 @@ class Book(models.Model):
 	course_usedin = models.IntegerField();
 	amazon_price = models.DecimalField(max_digits = 5, decimal_places = 			2)
 	labyrinth_price = models.DecimalField(max_digits = 5, decimal_places 			= 2)
-	lowest_studentprice = models.DecimalField(max_digit=5,decimal_places 			= 2)
+	lowest_studentprice = models.DecimalField(max_digits=5,decimal_places 	= 2)
 
 # Create your models here.
 
