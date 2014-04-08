fb367853b7dbf51b3e483051cf9e1ba41485d208
Alan <ayzhou@princeton.edu>
2014-04-07 22:22:46 -0400

    added michael's database

--- ptx2/settings.py
+++ ptx2/settings.py
@@ -14,6 +14,14 @@ DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
+
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    TEMPLATE_PATH,
+)
 
 
 # Quick-start development settings - unsuitable for production
--- ptx2app/views.py
+++ ptx2app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+
 
 # Create your views here.
+def index(request):
+	context = RequestContext(request)
+	
+	context_dict = {'boldmessage': This is a book!'}
+
+	return render_to_response('ptonptx2/index.html', context_dict, context)
+
