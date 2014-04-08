713d703fa1fbb29bd808057df7f9df107e0f23ab
Alan <ayzhou@princeton.edu>
2014-04-08 11:50:51 -0400

    added some urls

--- ptx2app/views.py
+++ ptx2app/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 from django.template import RequestContext
 from django.shortcuts import render_to_response
+from ptx2app.models import *
 
 
 # Create your views here.
 def index(request):
 	context = RequestContext(request)
 	
-	context_dict = {'boldmessage': This is a book!'}
+	user_list = User.objects.all()
+	context_dict = {'users' : user_list}
 
 	return render_to_response('ptonptx2/index.html', context_dict, context)
 
