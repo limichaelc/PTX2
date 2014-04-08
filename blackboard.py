2a6583b6321a10453f3a89820afab5ad819d60e6
Barbara <barbaraz@princeton.edu>
2014-04-08 00:47:55 -0400

    scraper - neds course used in, author, edition, and amazon price

--- blackboard.py
+++ blackboard.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 from time import sleep 
 import re
 import bbscrape
+import pyisbn
 
 BASE_URL = "http://www.amazon.com/gp/product/"
 BASE_2 = "http://www.labyrinthbooks.com/all_detail.aspx?isbn="
@@ -29,8 +30,22 @@ def get_books_page(url):
     return text
 
 def get_labyrinth_price():
-    txt = text.findAll(class_="textElementRight")
-    print txt
+    txt = text.findAll(class_="shoppingCartInfo")
+    prices = []
+    for j in txt:
+        price = j.find(class_="textElementRight")
+        if price != None:
+            price = str(price.contents).split('$')[1]
+            prices.append(price)
+    return prices
+
+#get list of titles and isbn numbers (alternating) from blackboard
+def get_titles_isbn():
+    txt = text.findAll('td', {'width':'100%', 'class':'textBold'})
+    list = []
+    for i in txt:
+        list.append(i.text)
+    return list
 
 def main():
     page = open('page.txt')
@@ -38,6 +53,7 @@ def main():
     table = soup.find('table')
     rows = table.findAll('tr')
     length = len(rows)
+    #for each class
     for i in range(1, length):
         currentrow = []
         thisrow = rows[i]
@@ -54,9 +70,36 @@ def main():
         
         currentrow.append(course_desig)
         currentrow.append(columns[2].contents[0])
-        #get page url
+        
+        #get page url for reading lists
         pageurl = columns[11].find('a')['href']
         text = get_books_page(pageurl)
-        print get_labyrinth_price()
         
+        #get the required books information for this class
+        thiscoursesbooks = []
+           #all labyrinth prices
+        lab_prices = get_labyrinth_price()
+        print lab_prices
+           #all titles and isbns(alterating)
+        titles = get_titles_isbn()
+        print titles
+        numbooks = len(lab_prices)
+        print "books " + str(numbooks)
+        n = 0
+        while n < numbooks:
+            #each book is a dictionary that contains:
+            #titles, isbn10, isbn13, lab_price, amazonprice, edition,
+            #author, course used in 
+            thisbook = []
+            thisbook.append(titles[2*n])
+            isbn10 = titles[(2*n)+1]
+            print isbn10
+            thisbook.append(isbn10)
+            isbn13 = pyisbn.convert(isbn10)
+            thisbook.append(isbn13)
+            thisbook.append(lab_prices[n])
+            #amazon prices
+            
+            n = n+1
+
 main()
