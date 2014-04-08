c972e98ff54714e488d6dc552c0c371354e6649b
Alan <ayzhou@princeton.edu>
2014-04-07 22:24:03 -0400

    added michael's database1

--- ptx2app/admin.py
+++ ptx2app/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from ptx2app.models import User, Book
+from ptx2app.models import User, Book, Author, Department, Course, Listing, Reading
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(Department)
+admin.site.register(Course)
+admin.site.register(Listing)
+admin.site.register(Reading)
