47c7221c0bcda15039125376f52585b2d5e14862
limichaelc <li.michael.c@gmail.com>
2014-04-09 23:58:01 -0400

    Removed useless stuff

--- books/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def index(request):
-    return HttpResponse("Books says hello world!")
-
-def about(request):
-    return HttpResponse("This is the about page!")
