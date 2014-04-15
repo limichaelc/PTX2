2073622cda9b132ab997882c35d73824cc616170
Alan <ayzhou@princeton.edu>
2014-04-15 11:57:21 -0400

    about page url

--- ptx2/settings.py
+++ ptx2/settings.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 
 import dj_database_url  # add this to requirements.txt
 DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
+DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
@@ -86,6 +87,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
 
 import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
