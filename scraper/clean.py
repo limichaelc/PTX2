50fa73ab952412517be598c9ff1250b184c9e765
Barbara <barbaraz@princeton.edu>
2014-04-15 13:32:18 -0400

    final stuff

---
+++ scraper/clean.py
@@ -0,0 +1,31 @@
+import re
+import json
+
+# used to remove duplicate courses (especially languages)
+
+data = open('finalcopy.txt', 'r')
+dictionary = eval(data.read())
+data.close()
+
+def main():
+    previous = []
+    newdict = []
+   # p = 0
+    for i in dictionary:
+        if i.get('coursedesig') == previous:
+           # p += 1
+            continue
+        #print i.get('booklist')
+        #if len(i.get('booklist')) != 0:
+         #   if i.get('booklist')[0] == 'error':
+                #print p
+        #p +=1
+        #if len(i.get('booklist')) > 0:
+        print i.get('coursename')
+            #print i.get('booklist')[0].get('title')
+        newdict.append(i)
+        previous = i.get('coursedesig')
+    #save copy of unduplicated dictionary
+    with open('data.json', 'wb') as fp:
+        json.dump(newdict, fp)
+main()
