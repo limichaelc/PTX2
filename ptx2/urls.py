049dec6eb12f4d7a912c47b7c61ca8d7760522cc
limichaelc <li.michael.c@gmail.com>
2014-04-14 21:31:51 -0400

    More edits to templates

--- ptx2/urls.py
+++ ptx2/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from ptx2.views import current_datetime, course_lookup, book_lookup
+from ptx2.views import *
 
 from django.contrib import admin
 admin.autodiscover()
@@ -12,7 +12,7 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 	url(r'^index/', 'ptx2app.views.index', name = 'index'),
 	url(r'^$', 'ptx2app.views.index', name = 'index'),
-	url(r'^time/$', current_datetime),
 	url(r'^courses/(.{3})/(\d{3})/$', course_lookup),
-	url(r'^book/', book_lookup),
+	url(r'^bookshelf/', bookshelf),
+	url(r'^test/', sidebar),
 )
--- ptx2/views.py
+++ ptx2/views.py
@@ -14,8 +14,13 @@ def course_lookup(request, dept, code):
 	html = "<html><body>You are looking at course %s %s.</body></html>" % (dept.upper(), code)
 	return HttpResponse(html)
 
-def book_lookup(request):
-	testbook = {'title': 'The Practice of Programming',
+def sidebar(request):
+	cos333 = {'dept': 'cos', 'code': 333, 'books': 1}
+	person = {'books_total': 4, 'books_needed': 2, 'courses': [cos333]}
+	return render(request, 'sidebar.html', {'person': person})
+
+def bookshelf(request):
+	needed_book1 = {'title': 'The Practice of Programming',
 	'isbn': '9780201615869',
 	'course_usedin': {'dept': 'COS', 'course_code': '333'},
 	'labyrinth_price': '15.00',
@@ -23,5 +28,21 @@ def book_lookup(request):
 	'student_price': '13.10',
 	'best_price': '12.20',
 	'best_seller': 'Amazon'}
-	testperson = {'book': {'owned': False }}
-	return render(request, 'book_lookup.html', {'book': testbook, 'person': testperson})
+	needed_book2 = {'title': 'Sen to Chihiro no Kamikakushi',
+	'isbn': '9784198614065',
+	'course_usedin': {'dept': 'JPN', 'course_code': '302'},
+	'labyrinth_price': '19.60',
+	'amazon_price': '8.60',
+	'student_price': '0',
+	'best_price': '8.60',
+	'best_seller': 'Amazon'}
+	selling_book1 = {'title': 'Language Files',
+	'isbn': '9780814251799',
+	'course_usedin': {'dept': 'LIN', 'course_code': '201'},
+	'selling_for': '40.00'}
+	owned_book1 = {'title': 'Kiite Oboeru Hanashikata Nihongo Nama Chukei Chu-Jokyu Hen',
+	'isbn': '9784874243008',
+	'course_usedin': {'dept': 'JPN', 'course_code': '302'}}
+	cos333 = {'dept': 'cos', 'code': 333, 'books': 1}
+	person = {'books_total': 4, 'books_needed': 2, 'books_selling': 1, 'books_owned': 1, 'courses': [cos333], 'first_name': 'Michael', 'last_name': 'Li'}
+	return render(request, 'bookshelf.html', {'books_needed': [needed_book1, needed_book2], 'books_selling': [selling_book1], 'books_owned': [owned_book1], 'person': person })
