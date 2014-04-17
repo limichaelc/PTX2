309a40384a811db64e83d921808f43f6f3e7c32b
limichaelc <li.michael.c@gmail.com>
2014-04-17 15:59:53 -0400

    Ignore DS_Store, fixed urls

--- ptx2/urls.py
+++ ptx2/urls.py
@@ -17,8 +17,4 @@ urlpatterns = patterns('',
     url(r'^sellbook/', 'ptx2app.views.sell_book', name = 'sell_book'),
     
 	#url(r'^courses/(?P<dept>\w+)/(?P<num>\d+)/$', 'ptx2app.templates.coursepage'),
-<<<<<<< HEAD
 )
-=======
-)
->>>>>>> FETCH_HEAD
