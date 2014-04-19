4330952947283fdf87bb0a30a4461f610b012d8a
Alan <ayzhou@princeton.edu>
2014-04-19 02:51:22 -0400

    added base url for future use

--- ptx2/settings.py
+++ ptx2/settings.py
@@ -83,6 +83,13 @@ WSGI_APPLICATION = 'ptx2.wsgi.application'
 AUTH_PROFILE_MODULE = 'ptx2app.Profile'
 
 TEMPLATE_CONTEXT_PROCESSORS = (
+   "django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.static",
+    "django.core.context_processors.tz",
+    "django.contrib.messages.context_processors.messages",
 
 	"ptx2.context_processors.baseurl"
 )
