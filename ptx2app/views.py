229dd15602b8aceabab1ae9d7da4ee116b57141e
Alan <ayzhou@princeton.edu>
2014-04-20 22:10:44 -0400

    trying to set current user to user

--- ptx2app/views.py
+++ ptx2app/views.py
@@ -65,11 +65,8 @@ def profile(request):
         else:
             print form.errors
     else:
-        form = ProfileForm()
-        form = form.save(commit=False)
-        form.user = request.user
-        form.save()
-
+        form = ProfileForm.objects.get(user = request.user)
+        
     return render_to_response('forms/newprofile.html', {'form': form}, context)
     
 def scrape(request):
