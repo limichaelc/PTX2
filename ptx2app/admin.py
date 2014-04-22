87ee1ceb72d4ba8be2e00795faf44bb1977b42eb
Alan <ayzhou@princeton.edu>
2014-04-21 20:36:18 -0400

    fixed a bug in models

--- ptx2app/admin.py
+++ ptx2app/admin.py
@@ -2,12 +2,10 @@ from django.contrib import admin
 from ptx2app.models import *
 
 # Register your models here.
-admin.site.register(Author)
 admin.site.register(Course)
 admin.site.register(Book)
 admin.site.register(PhysBook)
 admin.site.register(Review)
 admin.site.register(Profile)
 admin.site.register(Listing)
-admin.site.register(Reading)
 admin.site.register(Transaction)
