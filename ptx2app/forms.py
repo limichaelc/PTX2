37d49a3e6c2ec2fa844c328f1254f4f8be23147e
Alan <ayzhou@princeton.edu>
2014-04-21 20:51:53 -0400

    fixed a bug in models

--- ptx2app/forms.py
+++ ptx2app/forms.py
@@ -23,8 +23,8 @@ class SellBookForm(forms.ModelForm):
         
 class ProfileForm(forms.ModelForm):
 	books_needed = forms.ModelChoiceField(queryset = Book.objects.all())
-	books_owned =  forms.ModelChoiceField(queryset = Book.objects.all())
-	books_selling =  forms.ModelChoiceField(queryset = Book.objects.all())
+	books_owned =  forms.ModelChoiceField(queryset = PhysBook.objects.all())
+	books_selling =  forms.ModelChoiceField(queryset = PhysBook.objects.all())
 	course_list =  forms.ModelChoiceField(queryset = Course.objects.all())
 	
 	class Meta:
