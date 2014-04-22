b6fbac69b17dceb571d7646559a91bf8ef568bbf
Alan <ayzhou@princeton.edu>
2014-04-21 20:09:55 -0400

    updated models

--- ptx2app/models.py
+++ ptx2app/models.py
@@ -2,11 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class Author(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=40)
-    def __unicode__(self):
-        return self.last_name + self.first_name
 
 class Course(models.Model):
     name = models.CharField(max_length = 100)
@@ -28,7 +23,7 @@ class Book(models.Model):
     isbn10 = models.CharField(max_length=10)
     title = models.CharField(max_length=100)
     edition = models.CharField(max_length = 20, blank=True)
-    authors = models.ManyToManyField(Author)
+    authors = models.CharField(max_length = 100, blank = True)
     amazon_price = models.DecimalField(max_digits = 100, decimal_places = 2)
     labyrinth_price = models.DecimalField(max_digits = 100, decimal_places = 2)
     lowest_student_price = models.IntegerField()
@@ -38,20 +33,12 @@ class Book(models.Model):
 
 class PhysBook(models.Model):
     book = models.ForeignKey(Book)
-    missing_cover = models.BooleanField()
-    bent_corners = models.BooleanField()
-    has_markings = models.BooleanField()
-    has_scratches = models.BooleanField()
-    has_missing_pages = models.BooleanField()
-    has_creases = models.BooleanField()
     comment = models.CharField(max_length = 500, blank=True)
     def __unicode__(self):
         return self.book.title
 
 class Review(models.Model):
-    on_time = models.BooleanField()
-    right_price = models.BooleanField()
-    as_advertised = models.BooleanField()
+	comment = models.CharField(max_length = 500, blank = True)
 
 class Profile(models.Model):
     user = models.ForeignKey(User)
@@ -79,15 +66,9 @@ class Listing(models.Model):
     def __unicode__(self):
         return self.book.book.title + " for $" + str(self.price)
 
-class Reading(models.Model):
-    book = models.ForeignKey(Book)
-    is_recommended = models.BooleanField()
-    used_in = models.ManyToManyField(Course)
-    def __unicode__(self):
-        return self.book.__unicode__() + ' (recommended: ' + str(self.is_recommended) + ')'
-
 class Transaction(models.Model):
     buyer = models.ForeignKey(Profile, related_name = "transcation_buyer")
     seller = models.ForeignKey(Profile, related_name = "transcation_seller")
     book = models.ForeignKey(PhysBook)
     price = models.DecimalField(max_digits = 3, decimal_places = 2)
+    review = models.ManyToManyField(Review)
