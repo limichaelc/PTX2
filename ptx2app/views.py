f426cf64b85d9b771e8794707280fba3e2c6ee61
limichaelc <li.michael.c@gmail.com>
2014-04-24 16:48:06 -0400

    fixed handling of listings on bookshelf

--- ptx2app/views.py
+++ ptx2app/views.py
@@ -31,7 +31,7 @@ def get_context(request):
                     current_course.append(book.book)
         nums_by_course[course] = len(current_course)
     user_selling = []
-    for listing in Listings.():
+    for listing in Listings.all():
         if listing.owner == profile:
             user_selling.append(listing)
 
