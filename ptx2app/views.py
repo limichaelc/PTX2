7ce8f7c96183efc39821e6611d00c775306326ef
Alan <ayzhou@princeton.edu>
2014-04-28 20:34:07 -0400

    confirm purchase page

--- ptx2app/views.py
+++ ptx2app/views.py
@@ -239,5 +239,8 @@ def confirmbuybook(request, isbn, listingid):
 	
     context_dict['listing'] = listing
     context_dict['sellerprofile'] = sellerprofile
+    
+    transaction = Transaction(buyer = context_dict['user'], seller=listing.owner, price=listing.price, book = listing.book)
+    transaction.save()
 	
     return render_to_response('ptonptx2/afterpurchase.html', context_dict, context)
