90b97a324b55301e4d9a5c24b56813f5ebcd5bf0
Alan <ayzhou@princeton.edu>
2014-04-28 22:06:59 -0400

    models edit

--- ptx2app/views.py
+++ ptx2app/views.py
@@ -240,7 +240,7 @@ def confirmbuybook(request, isbn, listingid):
     context_dict['listing'] = listing
     context_dict['sellerprofile'] = sellerprofile
     
-    transaction = Transaction(buyer = context_dict['user'], seller=listing.owner, price=listing.price, book = listing.book)
+    transaction = Transaction(buyer = request.user.get_profile(), seller=listing.owner, price=listing.price, book = listing.book)
     transaction.save()
 	
     return render_to_response('ptonptx2/afterpurchase.html', context_dict, context)
