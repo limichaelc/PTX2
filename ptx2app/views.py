330f656154ecb37cebc7fd8d57e4480da387e3b8
Barbara <barbaraz@princeton.edu>
2014-04-28 22:41:59 -0400

    search

--- ptx2app/views.py
+++ ptx2app/views.py
@@ -171,9 +171,12 @@ def history(request):
     
     return render_to_response('ptonptx2/history.html', context_dict, context)
 
+def search_form(request):
+    return render(request, 'nav.html')
+
 def search(request):
-    if q in request.GET:
-        message = 'You searched for: %r' % request.GET[q]
+    if request.GET['q']:
+        message = 'You searched for: %r' % request.GET['q']
     else:
         message = 'You submitted an empty form.'
     return HttpResponse(message)
