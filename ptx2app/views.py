070e8f4ac56ee5479ff3a4e5b72b0edb933f4e53
Alan <ayzhou@princeton.edu>
2014-04-30 21:51:02 -0400

    removecourse

--- ptx2app/views.py
+++ ptx2app/views.py
@@ -318,9 +318,7 @@ def pendingtransaction(request, id):
 
     if request.method == 'POST':
         form = ReviewForm(request.POST)
-        return HttpResponse("Welcome to the page at /current/")
         if form.is_valid():
-            return index(request)
             form = form.save()    
             transaction = Transaction.objects.get(id=id)
             if transaction.buyer == context_dict['user']:
