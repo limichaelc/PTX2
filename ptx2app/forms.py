2d088f3fa7b241336978f69fee76f4710182c1eb
Alan <ayzhou@princeton.edu>
2014-05-01 00:28:54 -0400

    sell

--- ptx2app/forms.py
+++ ptx2app/forms.py
@@ -38,8 +38,8 @@ class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
 
-class PhysBookForm(forms.Form):
+class PhysBookForm(forms.ModelForm):
 	class Meta:
 		model = PhysBook
-		exclude= ['book']
+		exclude = ['book']
 
