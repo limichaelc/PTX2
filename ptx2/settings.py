2b9a390e1cfd525791f5eb2a30f0da368a6b7712
limichaelc <li.michael.c@gmail.com>
2014-05-08 10:17:35 -0400

    Turned off debuging

--- ptx2/settings.py
+++ ptx2/settings.py
@@ -40,9 +40,9 @@ TEMPLATE_DIRS = (
 SECRET_KEY = 'f+n&gf!ph09l@9w=vy5%^@zl8lf4bzc12^#d(^s*5bhcou&xub'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-TEMPLATE_DEBUG = True
+TEMPLATE_DEBUG = False
 
 ALLOWED_HOSTS = []
 
