c8b219cc2198f37f4e8e98c1a7a7fbafba69d877
mtengi <markat@princeton.edu>
2014-05-08 05:05:45 -0400

    autofocus in input boxes in modals

--- ptx2app/views.py
+++ ptx2app/views.py
@@ -20,7 +20,6 @@ from django.core.mail import send_mail
 # get the context of this request
 def get_context(request):
     context = RequestContext(request)
-    first=False
     try:
         user = request.user
     except:
@@ -32,8 +31,8 @@ def get_context(request):
         profile = Profile.objects.create(user = user)
         #messages.success(request, 
             #'<strong>Welcome!</strong> This appears to be your first visit. You can get started by <a href="#" data-toggle="modal" data-target="#newcoursemodal" class="alert-link">adding a new course</a>.')
-        first=True
         profile.save()
+    first = (not profile.first_name)
     books = Book.objects.all()
     transaction = Transaction.objects.all()
 
