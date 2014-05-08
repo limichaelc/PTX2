bdd68f4a71403d0c3651c7334378570389cada62
Alan <ayzhou@princeton.edu>
2014-05-07 20:28:51 -0400

    email

--- ptx2app/views.py
+++ ptx2app/views.py
@@ -651,8 +651,8 @@ def pendingtransaction(request, id):
                 buyer = transaction.buyer
                 seller = transaction.seller
             	buyer.books_owned.add(book)
-                sellermessage = "Hi! \n Someone has bought your book on PTX2. The user's details are: \n Name:" + buyer.first_name + " " + buyer.last_name + "\n Buyer email: " + buyer.user.username +"@princeton.edu \n Preferred meeting place: " + buyer.preferred_meetingplace + "\n Thanks, \n\n PTX2"
-            	buyermessage = "Hi! \n You have bought someone else's book on PTX2. The user's details are: \n Name:" + seller.first_name + " " + seller.last_name + "\n Seller email: " + seller.user.username +"@princeton.edu \n Preferred meeting place: " +seller.preferred_meetingplace + "\n Thanks, \n\n PTX2"
+                sellermessage = "Hi! \n\n Someone has bought your book on PTX2. The user's details are: \n Name:" + buyer.first_name + " " + buyer.last_name + "\n Buyer email: " + buyer.user.username +"@princeton.edu \n Preferred meeting place: " + buyer.preferred_meetingplace + "\n\n Thanks, \n\n PTX2"
+            	buyermessage = "Hi! \n You have bought someone else's book on PTX2. The user's details are: \n Name:" + seller.first_name + " " + seller.last_name + "\n Seller email: " + seller.user.username +"@princeton.edu \n Preferred meeting place: " +seller.preferred_meetingplace + "\n\n Thanks, \n\n PTX2"
             	send_mail('Pending transaction', sellermessage, 'princetonptx2@gmail.com', [seller.user.username + '@princeton.edu'], fail_silently=False)
             	send_mail('Pending transaction', buyermessage, 'princetonptx2@gmail.com', [buyer.user.username + '@princeton.edu'], fail_silently=False)
             	listing.delete()
