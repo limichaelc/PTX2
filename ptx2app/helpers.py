0d57ac4819a07a2d6ef396798cd7130797eeb08a
mtengi <markat@princeton.edu>
2014-05-08 01:52:28 -0400

    fixed bug in set_lowest_price

--- ptx2app/helpers.py
+++ ptx2app/helpers.py
@@ -5,7 +5,7 @@ def set_lowest_price(book):
         book.lowest_student_price = None
     else:
         book.lowest_student_price = 1000000
-        for listing in Listing.objects.filter(book__book__pk = book.pk):
+        for listing in Listing.objects.filter(book__book__pk = book.pk, sell_status='O'):
             if listing.price < book.lowest_student_price:
                 book.lowest_student_price = listing.price
     book.save()
